5130020a582cedd9d8b22cb3bd2e0dacc374e200
RamiYacoub <ramiaref61@gmail.com>
2021-11-09 00:51:50 +0200

    hello

--- app/src/main/java/com/example/ass1final/MainActivity.java
+++ app/src/main/java/com/example/ass1final/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.ass1final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        Button button1 =(Button)findViewById(R.id.login);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        Button button2 =(Button)findViewById(R.id.CreateNewAccountButton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/ass1final/RegisterActivity.java
+++ app/src/main/java/com/example/ass1final/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.ass1final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button button =(Button)findViewById(R.id.returnFromRegister);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
