d88d7c68bc1e2b2f405137ac369d5d9683bd0122
RamiYacoub <ramiaref61@gmail.com>
2021-11-09 02:43:11 +0200

    hello

--- app/src/main/java/com/example/ass1final/RegisterActivity.java
+++ app/src/main/java/com/example/ass1final/RegisterActivity.java
@@ -2,10 +2,12 @@ package com.example.ass1final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -20,6 +22,20 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+
+        });
+
+        Button button1 =(Button)findViewById(R.id.button3);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                Context context = getApplicationContext();
+                CharSequence text = "Register successfully";
+                int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(context, text, duration).show();
             }
 
         });
